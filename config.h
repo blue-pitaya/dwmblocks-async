3596799fc8cb515f702435c2d4bf2ed6fdc08c74
blue-pitaya <blue.pitaya@pm.me>
2022-09-11 18:48:37 +0200

    updated blocks and added install script

--- config.h
+++ config.h
@@ -3,14 +3,8 @@
 #define CLICKABLE_BLOCKS
 
 const Block blocks[] = {
-	BLOCK("sb-mail",    1800, 17),
-	BLOCK("sb-music",   0,    18),
-	BLOCK("sb-disk",    1800, 19),
-	BLOCK("sb-memory",  10,   20),
-	BLOCK("sb-loadavg", 5,    21),
-	BLOCK("sb-mic",     0,    26),
-	BLOCK("sb-record",  0,    27),
-	BLOCK("sb-volume",  0,    22),
-	BLOCK("sb-battery", 5,    23),
-	BLOCK("sb-date",    1,    24)
+	BLOCK("headset-battery",  5, 9),
+	BLOCK("sound-volume",     30, 10),
+	BLOCK("ram-usage",        1, 11),
+	BLOCK("show-time",        1, 12),
 };
